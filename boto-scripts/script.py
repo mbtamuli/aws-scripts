08109d6d2b4c74fce5d700a59a780fc6eec0ec84
Mriyam Tamuli <mbtamuli@gmail.com>
2017-08-25 22:35:54 +0530

    Current script
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- boto-scripts/script.py
+++ boto-scripts/script.py
@@ -6,11 +6,53 @@ from pprint import pprint
 
 ec = boto3.client('ec2')
 retention_days = 7
-reservations = ec.describe_instances(
-    Filters=[
+def lambda_handler(event, context):
+
+    reservations = ec.describe_instances(
+        Filters=[
             {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
         ]
-)
-pprint (reservations['Reservations'][0])
-# for instance in reservations['Reservations']:
-#     print (type(instance))
+    ).get(
+        'Reservations', []
+    )
+    print (reservations)
+
+    instances = sum(
+        [
+            [i for i in r['Instances']]
+            for r in reservations
+
+        ], [])
+
+    print "Found %d instances that need backing up" % len(instances)
+
+    to_tag = collections.defaultdict(list)
+
+    for instance in instances:
+            name_tag = [
+                str(t.get('Value')) for t in instance['Tags']
+                if t['Key'] == 'Name'][0]
+            print (name_tag)
+            print ("check_loop")
+
+            create_time = datetime.datetime.now()
+            create_fmt = create_time.strftime('%Y-%m-%d')
+
+            AMIid = ec.create_image(InstanceId=instance['InstanceId'], Name="Lambda - " + instance['InstanceId'] + " " + name_tag +" from " + create_fmt, Description="Lambda created AMI of instance " + instance['InstanceId'] + " " + name_tag + " from " + create_fmt, NoReboot=True, DryRun=False)
+            to_tag[retention_days].append(AMIid['ImageId'])
+
+            delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+            delete_fmt = delete_date.strftime('%m-%d-%Y')
+
+            ec.create_tags(
+                Resources=to_tag[retention_days],
+                Tags=[
+                        {'Key': 'DeleteOn', 'Value': delete_fmt},
+
+                    ]
+            )
+
+
+            print ("Name tag " + name_tag)
+            print ("check_loopend")
+
